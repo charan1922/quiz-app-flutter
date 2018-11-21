0d76aa6807dba055bfe0bc000e8c8b68906adbf1
Charan Teja <charan.teja@cashapona.com>
2018-11-21 23:59:04 +0530

    updated code

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
 import 'models/image_model.dart';
 import 'dart:convert';
+import './widgets/image_list.dart';
 
 void main() => runApp(MyApp());
 
@@ -58,8 +59,6 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       images.add(imageModel);
     });
-    print(images);
-    print("sdf");
   }
 
   @override
@@ -76,32 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many time:',
-            ),
-          ],
-        ),
-      ),
+      body: ImageList(images),
       floatingActionButton: FloatingActionButton(
         onPressed: fetchImage,
         tooltip: 'Increment',
