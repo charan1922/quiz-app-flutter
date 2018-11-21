c07c01b5b9e78365c696a6bd535c02d8a1355819
Charan Teja <charan.teja@cashapona.com>
2018-11-22 01:47:28 +0530

    added form fields

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            passwordField(),
+            Container(
+              margin: EdgeInsets.only(bottom: 20.0),
+            ),
+            submitButton()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        hintText: 'c@c.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'password',
+        labelText: 'password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text("submit"),
+      color: Colors.blueAccent,
+      onPressed: () {},
+    );
+  }
+}
